e83efce626b9c0459cba7b148c2af57a30e4a553
Steff Gornall <steffgornall@gmail.com>
2024-08-18 23:16:13 +0100

    Remove sign-in wall for testing

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
     <div className="bg-opacity flex h-full w-full flex-col items-center gap-10 bg-stone-100 dark:bg-slate-700">
       <Navbar />
       {/* <Functionality /> */}
-      <SignedOut>
+      {/* <SignedOut>
         <WelcomePage />
-      </SignedOut>
-      <SignedIn>
-        <DuesCalculator />
-        <Calculator />
-      </SignedIn>
+      </SignedOut> */}
+      {/* <SignedIn> */}
+      <DuesCalculator />
+      <Calculator />
+      {/* </SignedIn> */}
       <Footer />
     </div>
   );
