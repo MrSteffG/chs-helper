3fc2357f95a2c876300e6902b67359955b663add
Steff Gornall <steffgornall@gmail.com>
2024-08-18 13:57:51 +0100

    Dues calculator functional

--- app/components/DuesCalculator.tsx
+++ app/components/DuesCalculator.tsx
@@ -1,28 +1,106 @@
 import React from "react";
 
 const DuesCalculator = () => {
+  //Prices
+  const adultBouldering = 44;
+  const concessionBouldering = 33;
+  const adultSport = 52;
+  const concessionSport = 40;
+  const addon = 16;
+
+  const todaysDate = new Date();
+  const todaysDay = todaysDate.getDate();
+
+  const thisMonth = todaysDate.getMonth();
+  const nextMonth = thisMonth + 1;
+
+  const daysInMonth = (month: number) => {
+    switch (month) {
+      case 0:
+        return 31;
+      case 1:
+        return 28;
+      case 2:
+        return 31;
+      case 3:
+        return 30;
+      case 4:
+        return 31;
+      case 5:
+        return 30;
+      case 6:
+        return 31;
+      case 7:
+        return 31;
+      case 8:
+        return 30;
+      case 9:
+        return 31;
+      case 10:
+        return 30;
+      case 11:
+        return 31;
+    }
+  };
+
+  const thisMonthDays = daysInMonth(thisMonth);
+  const nextMonthDays: any = daysInMonth(nextMonth);
+  //Add extra day to account for today
+  const daysToEndOfNextMonth = nextMonthDays - todaysDay + 1;
+
+  const duesMultiplier = (membershipType: number) => {
+    return (
+      Math.round(
+        (membershipType / nextMonthDays) * daysToEndOfNextMonth * 100,
+      ) / 100
+    );
+  };
+
   return (
     <div className="flex w-2/3 flex-col items-center justify-center gap-5 rounded-2xl bg-slate-600 p-10 text-white">
-      <h3 className="flex">Todays Date is {Date()}</h3>
-      <div className="flex w-2/3 items-center justify-around rounded-2xl">
+      <h3 className="flex gap-5">Todays Date is {todaysDate.toString()}</h3>
+      <h3>Date logic tests</h3>
+      <p>thisMonth = {thisMonth}</p>
+      <p>thisMonthDays = {thisMonthDays}</p>
+      <p>nextMonth = {nextMonth}</p>
+      <p>nextMonthDays = {nextMonthDays}</p>
+      <p>todaysDay = {todaysDay}</p>
+      <p>daysToEndOfNextMonth = {daysToEndOfNextMonth}</p>
+
+      <div className="flex w-4/5 items-center justify-around rounded-2xl">
         <div className="flex flex-col items-center justify-center">
           Bouldering
           <div className="flex gap-5">
-            <div className="flex">Adult</div>
-            <div className="flex">Under 18 / Concession</div>
+            <div className="flex flex-col">
+              <h3>Adult</h3>
+              <p>{duesMultiplier(adultBouldering)}</p>
+            </div>
+            <div className="flex flex-col">
+              <h3>Under 18 / Concession</h3>
+              <p>{duesMultiplier(concessionBouldering)}</p>
+            </div>
           </div>
         </div>
         <div className="flex flex-col items-center justify-center">
           Sport
           <div className="flex gap-5">
-            <div className="flex">Adult</div>
-            <div className="flex">Under 18 / Concession</div>
+            <div className="flex flex-col">
+              <h3>Adult</h3>
+              <p>{duesMultiplier(adultSport)}</p>
+            </div>
+            <div className="flex flex-col">
+              <h3>Under 18 / Concession</h3>
+              <p>{duesMultiplier(concessionSport)}</p>
+            </div>
           </div>
         </div>
         <div className="flex flex-col items-center justify-center">
           Add-on
           <div className="flex gap-5">
-            <div className="flex">Under 18 / NICAS</div>
+            <div className="flex flex-col">
+              <h3>Under 18 / NICAS</h3>
+              <p>{duesMultiplier(addon)}</p>
+            </div>
           </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ import DuesCalculator from "./components/DuesCalculator";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-full flex-col items-center gap-10 bg-slate-700">
+    <div className="flex h-full w-full flex-col items-center gap-10 bg-slate-700">
       <Navbar />
       {/* <Functionality /> */}
       <DuesCalculator />
       <Calculator />
+      footer
     </div>
   );
 }
