0c1ef9d77055b8865e3129e5da51ddd8169d1c28
Steff Gornall <steffgornall@gmail.com>
2024-09-12 12:24:44 +0100

    Add staff wage input

--- app/components/Calculator.tsx
+++ app/components/Calculator.tsx
@@ -20,6 +20,7 @@ const Calculator = () => {
   const [sessionLength, setSessionLength] = useState(1);
   const [extraStaffRequired, setExtraStaffRequired] = useState(1);
   const [ratio, setRatio] = useState(twBoulderingAw);
+  const [staffWage, setStaffWage] = useState(10);
 
   // Input onChange functions
   const onChangeParticipants = (e: any) => {
@@ -36,8 +37,12 @@ const Calculator = () => {
     setExtraStaffRequired(e.target.value);
   };
 
+  const onChangeWage = (e: any) => {
+    setStaffWage(Math.round(e.target.value * 100) / 100);
+  };
+
   //example data
-  const staffWage = 10;
+
   const staffWageExtraCosts = staffWage * 0.3;
   const totalStaffWage = staffWage + staffWageExtraCosts;
   const averageShiftHours = 4;
@@ -45,12 +50,12 @@ const Calculator = () => {
   //Calculator vaiables
   let requiredInstructors = Math.ceil(participants / ratio);
   let staffSessionHours = requiredInstructors * sessionLength;
-  let sessionCost = staffSessionHours * totalStaffWage;
+  let sessionCost = Math.round(staffSessionHours * totalStaffWage * 100) / 100;
   let usedStaffHours = extraStaffRequired * sessionLength;
   let extraStaffHours = extraStaffRequired * averageShiftHours - usedStaffHours;
   let extraStaffHoursCost = extraStaffHours * totalStaffWage;
-  let totalCost = sessionCost + extraStaffHoursCost;
-  let totalCostProfit = totalCost * 1.5;
+  let totalCost = Math.round((sessionCost + extraStaffHoursCost) * 100) / 100;
+  let totalCostProfit = Math.round(totalCost * 1.5 * 100) / 100;
   let costPerParticipant =
     Math.round((totalCostProfit / participants) * 100) / 100;
 
@@ -118,6 +123,17 @@ const Calculator = () => {
             <option value="6">6</option>
           </select>
         </div>
+        <div className="flex justify-between gap-5 max-lg:flex-col">
+          <h3 className="font-semibold">Average Staff Wage</h3>
+
+          <input
+            type="number"
+            step="0.01"
+            className="w-96 rounded-lg bg-stone-100 p-1 pl-2 text-black dark:bg-slate-300 max-lg:w-full"
+            defaultValue={10}
+            onChange={onChangeWage}
+          />
+        </div>
         <div id="gap div" className="opacity-0">
           Value
         </div>
@@ -138,26 +154,26 @@ const Calculator = () => {
             <h3 className="flex justify-center bg-stone-200 text-center font-semibold dark:bg-slate-800 max-lg:w-full">
               Session Cost
             </h3>
-            <h3 className="p-3">£ {sessionCost}</h3>
+            <h3 className="p-3">£{sessionCost}</h3>
           </div>
           {/* extra staff hours */}
           <div className="flex flex-col items-center">
             <h3 className="flex justify-center bg-stone-200 text-center font-semibold dark:bg-slate-800 max-lg:w-full">
               Total Cost
             </h3>
-            <h3 className="p-3">£ {totalCost}</h3>
+            <h3 className="p-3">£{totalCost}</h3>
           </div>
           <div className="flex flex-col items-center">
             <h3 className="flex justify-center bg-stone-200 text-center font-semibold dark:bg-slate-800 max-lg:w-full">
               Total Cost with profit
             </h3>
-            <h3 className="p-3">£ {totalCostProfit}</h3>
+            <h3 className="p-3">£{totalCostProfit}</h3>
           </div>
           <div className="flex flex-col items-center">
             <h3 className="flex justify-center rounded-tr-xl bg-stone-200 text-center font-semibold dark:bg-slate-800 max-lg:w-full max-lg:rounded-none">
               Cost per participant
             </h3>
-            <h3 className="p-3">£ {costPerParticipant}</h3>
+            <h3 className="p-3">£{costPerParticipant}</h3>
           </div>
         </div>
       </div>
