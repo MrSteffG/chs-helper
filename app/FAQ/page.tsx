90b4855eb4628eaf4f1858162d6f055e59606eb0
Steff Gornall <steffgornall@gmail.com>
2024-09-12 22:13:13 +0100

    Add supervision post

--- app/FAQ/page.tsx
+++ app/FAQ/page.tsx
@@ -4,17 +4,20 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import ProcessingMemberships from "../articles/ProcessingMemberships.mdx";
+
+import * as Supervision from "../articles/Supervision.mdx";
+import * as Memberships from "../articles/ProcessingMemberships.mdx";
+
 import { useState } from "react";
 
 export default function FAQ() {
-  const Article = ({ title, file }: { title: string; file: any }) => {
+  const Article = (props: { title: any; file: any }) => {
     const [hidden, setHidden] = useState(true);
 
     return (
       <div className="flex w-2/3 flex-col items-center justify-start rounded-2xl border bg-white p-10 dark:bg-slate-600 max-lg:w-5/6">
         <div className="flex w-2/3 items-center justify-between gap-2 font-semibold">
-          <h3 className="text-2xl">{title}</h3>
+          <h3 className="text-2xl">{props.title}</h3>
           <IoIosArrowUp
             className={`${hidden === false ? " " : "hidden opacity-0"}text-xl hover:scale-105 hover:cursor-pointer`}
             onClick={() => setHidden(!hidden)}
@@ -27,7 +30,7 @@ export default function FAQ() {
         <article
           className={`prose ${hidden === false ? "" : "hidden"} prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:text-white dark:prose-headings:text-white dark:prose-strong:text-white`}
         >
-          <ProcessingMemberships />
+          <props.file />
         </article>
       </div>
     );
@@ -36,7 +39,8 @@ export default function FAQ() {
   return (
     <div className="bg-opacity flex h-full w-full flex-col items-center gap-10 bg-stone-100 dark:bg-slate-700">
       <Navbar />
-      <Article title="Membership Guide" file={ProcessingMemberships} />
+      <Article title={Supervision.title} file={Supervision.default} />
+      <Article title={Memberships.title} file={Memberships.default} />
       <Footer />
     </div>
   );
